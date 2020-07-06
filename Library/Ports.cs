da2433b8c3d152ba3286906eeed65240dc7babdc
Goldt, Jonas <jonas.goldt@siemens.com>
2020-07-06 12:20:49 +0200

    Initial commit

---
+++ Library/Ports.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Library
+{
+    public class Ports
+    {
+        public static void PrintPortNamesToConsole()
+        {
+            foreach(var port in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(port);
+            }
+        }
+    }
+}
---
+++ TestProjectLibReference/Tests.cs
@@ -0,0 +1,14 @@
+using Library;
+using NUnit.Framework;
+
+namespace TestProjectLibReference
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Ports.PrintPortNamesToConsole();
+        }
+    }
+}
